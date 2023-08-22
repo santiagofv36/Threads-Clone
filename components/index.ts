d6045eef5544909f9341d80da13e41c46dbde872
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2023-08-22 12:08:10 -0400

    Home Page
    Implemented the layout of the home page of the project

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Bottombar } from './shared/Bottombar';
+export {default as Topbar} from './shared/Topbar';
+export {default as RightSidebar} from './shared/RightSidebar';
+export {default as LeftSidebar} from './shared/LeftSidebar';
